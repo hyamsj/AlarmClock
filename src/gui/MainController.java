3ad5436a97f346368daf2b124d60822ff1d0ca9e
joni <jonathan.hyams@students.bfh.ch>
2017-02-28 15:29:42 +0100

    remove timer package

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -1,11 +1,22 @@
 package gui;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainController {
 
     private MainModel model;
 
+    Timer timer;
+    TimerTask task;
+
+    int secondsPassed = 0;
+    int max = 5;
+
     public MainController(MainModel m) {
         model = m;
+        createTimer();
+        startTimer();
     }
 
     public void increment() {
@@ -14,6 +25,29 @@ public class MainController {
 
     public void reset() {
         model.reset();
+
+    }
+
+    public void createTimer(){
+        this.timer = new Timer();
+        this.task = new TimerTask() {
+            @Override
+            public void run() {
+                secondsPassed++;
+                System.out.println("Seconds passed " + secondsPassed);
+                if (secondsPassed == max){
+                    timer.cancel();
+                    int i = Integer.MAX_VALUE;
+                    System.out.println(i);
+                }
+            }
+        };
+    }
+
+
+
+    public void startTimer(){
+        timer.scheduleAtFixedRate(task, 1000, 1000);
 
     }
 }
