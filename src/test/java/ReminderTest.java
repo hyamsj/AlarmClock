51c6d7b82e6b789f884e5365e05824843398a2f8
joni <jonathan.hyams@students.bfh.ch>
2017-05-02 17:37:50 +0200

    fixed test

--- src/test/java/ReminderTest.java
+++ src/test/java/ReminderTest.java
@@ -17,20 +17,19 @@ import java.time.LocalDateTime;
 public class ReminderTest {
     @Test
     public void getSubjectProperty() throws Exception {
-        String subject="titel";
-        String description="description fo the Reminder";
+        String subject = "titel";
+        String description = "description fo the Reminder";
         String time = "12:22";
-        LocalDate date = LocalDate.of(2017,3,4);
-
+        LocalDate date = LocalDate.of(2017, 3, 4);
 
 
         SimpleStringProperty subjectProperty = new SimpleStringProperty(subject);
 
 
-        Reminder reminder = new Reminder(subject,description,time,date);
+        Reminder reminder = new Reminder(subject, description, time, date);
         SimpleStringProperty subjectFromPropertyGetter = reminder.getSubjectProperty();
-//        assertSame(subjectProperty,subjectFromPropertyGetter;
-        assertTrue(subjectProperty.equals(subjectFromPropertyGetter));
+        assertEquals(subjectProperty.get(), subjectFromPropertyGetter.get());
+//        assertTrue(subjectProperty.equals(subjectFromPropertyGetter));
         //assertTrue(subjectProperty == subjectFromPropertyGetter);
 
     }
@@ -52,16 +51,16 @@ public class ReminderTest {
     }
 
     @Test
-    public void getSubject(){
-        String subject="titel";
-        String description="description fo the Reminder";
+    public void getSubject() {
+        String subject = "titel";
+        String description = "description fo the Reminder";
         String timeString = "12:22";
-        LocalDate dateTest = LocalDate.of(2017,3,4);
+        LocalDate dateTest = LocalDate.of(2017, 3, 4);
 
-        Reminder reminder = new Reminder(subject,description,timeString,dateTest);
+        Reminder reminder = new Reminder(subject, description, timeString, dateTest);
         String subjectFromGetter = reminder.getSubject();
 
-        assertEquals(subject,subjectFromGetter);
+        assertEquals(subject, subjectFromGetter);
 
         /*
         LocalDateTime currentTime = LocalDateTime.now();
@@ -73,52 +72,52 @@ public class ReminderTest {
     }
 
     @Test
-    public void getDescription(){
-        String subject="titel";
-        String description="description fo the Reminder";
+    public void getDescription() {
+        String subject = "titel";
+        String description = "description fo the Reminder";
         String timeString = "12:22";
-        LocalDate dateTest = LocalDate.of(2017,3,4);
+        LocalDate dateTest = LocalDate.of(2017, 3, 4);
 
-        Reminder reminder = new Reminder(subject,description,timeString,dateTest);
+        Reminder reminder = new Reminder(subject, description, timeString, dateTest);
         String descriptionFromGetter = reminder.getDescription();
 
-        assertEquals(description,descriptionFromGetter);
+        assertEquals(description, descriptionFromGetter);
     }
 
     @Test
-    public void getTime(){
-        String subject="titel";
-        String description="description fo the Reminder";
+    public void getTime() {
+        String subject = "titel";
+        String description = "description fo the Reminder";
         String timeString = "12:22";
-        LocalDate dateTest = LocalDate.of(2017,3,4);
+        LocalDate dateTest = LocalDate.of(2017, 3, 4);
 
-        Reminder reminder = new Reminder(subject,description,timeString,dateTest);
+        Reminder reminder = new Reminder(subject, description, timeString, dateTest);
         String timeFromGetter = reminder.getTime();
 
-        assertEquals(timeString,timeFromGetter);
+        assertEquals(timeString, timeFromGetter);
 
     }
 
     @Test
-    public void getDate(){
-        String subject="titel";
-        String description="description fo the Reminder";
+    public void getDate() {
+        String subject = "titel";
+        String description = "description fo the Reminder";
         String timeString = "12:22";
-        LocalDate dateTest = LocalDate.of(2017,3,4);
+        LocalDate dateTest = LocalDate.of(2017, 3, 4);
 
-        Reminder reminder = new Reminder(subject,description,timeString,dateTest);
-        LocalDate dateFromGetter= reminder.getDate();
+        Reminder reminder = new Reminder(subject, description, timeString, dateTest);
+        LocalDate dateFromGetter = reminder.getDate();
 
-        assertEquals(dateTest,dateFromGetter);
+        assertEquals(dateTest, dateFromGetter);
     }
 
     @Test
-    public void TestMinMaxDate(){
+    public void TestMinMaxDate() {
         LocalDateTime currentTime = LocalDateTime.now();
         LocalDate dateNow = currentTime.toLocalDate();
         LocalDate dateMAX = LocalDate.MAX;
         LocalDate dateMIN = LocalDate.MIN;
-        LocalDate dateManual = LocalDate.of(2017,2,13);
+        LocalDate dateManual = LocalDate.of(2017, 2, 13);
 
         ReminderGetDateMaxMin(dateNow);
         ReminderGetDateMaxMin(dateMAX);
@@ -128,17 +127,16 @@ public class ReminderTest {
 
 
     //gets called multiple times by TestMinMaxDate does NOT need a @Test annotation
-    public void ReminderGetDateMaxMin(LocalDate date){
+    public void ReminderGetDateMaxMin(LocalDate date) {
         //TODO test for max min too
-        String subject="titel";
-        String description="description fo the Reminder";
+        String subject = "titel";
+        String description = "description fo the Reminder";
         String timeString = "12:22";
 
-        Reminder reminder = new Reminder(subject,description,timeString,date);
-        LocalDate dateFromGetter= reminder.getDate();
-        assertEquals(date,dateFromGetter);
+        Reminder reminder = new Reminder(subject, description, timeString, date);
+        LocalDate dateFromGetter = reminder.getDate();
+        assertEquals(date, dateFromGetter);
     }
-
 
 
 }
