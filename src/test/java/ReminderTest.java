6aff0fd6ff5ee72da4e670fc87a7418f47c023c1
pascal <you@example.com>
2017-05-02 20:48:14 +0200

    added getter == proppertyGetter.get() tests

--- src/test/java/ReminderTest.java
+++ src/test/java/ReminderTest.java
@@ -6,6 +6,7 @@ import alarmClock.model.Reminder;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -15,6 +16,55 @@ import java.time.LocalDateTime;
  * Created by pascal on 5/2/17.
  */
 public class ReminderTest {
+    SimpleStringProperty descriptionProperty;
+    SimpleStringProperty timeProperty;
+    SimpleObjectProperty<LocalDate> dateProperty;
+    SimpleStringProperty subjectProperty;
+    Reminder reminder;
+
+    @Before
+    public void beforeTest(){
+        String subject = "titel";
+        String description = "description fo the Reminder";
+        String time = "12:22";
+        LocalDate date = LocalDate.of(2017, 3, 4);
+
+        descriptionProperty = new SimpleStringProperty("");
+        timeProperty = new SimpleStringProperty("");
+        dateProperty = new SimpleObjectProperty<>();
+        subjectProperty = new SimpleStringProperty(subject);
+        reminder = new Reminder(subject, description, time, date);
+
+    }
+    @Test
+    public void gettersReturnSameSubject(){
+        String subjectFromPropertyGetter = reminder.getSubjectProperty().get();
+        String subjectFromGetter = reminder.getSubject();
+        assertEquals(subjectFromGetter,subjectFromPropertyGetter);
+    }
+
+
+    @Test
+    public void gettersReturnSameDescription(){
+        String getProperty= reminder.getDescriptionProperty().get();
+        String get= reminder.getDescription();
+        assertEquals(get,getProperty);
+    }
+
+    @Test
+    public void gettersReturnSameTime(){
+        String getProperty= reminder.getTimeProperty().get();
+        String get= reminder.getTime();
+        assertEquals(get,getProperty);
+    }
+
+    @Test
+    public void gettersReturnSameDate(){
+        LocalDate getProperty= reminder.getDateProperty().get();
+        LocalDate get= reminder.getDate();
+        assertEquals(get,getProperty);
+    }
+
     @Test
     public void getSubjectProperty() throws Exception {
         String subject = "titel";
@@ -22,6 +72,9 @@ public class ReminderTest {
         String time = "12:22";
         LocalDate date = LocalDate.of(2017, 3, 4);
 
+        SimpleStringProperty descriptionProperty = new SimpleStringProperty("");
+        SimpleStringProperty timeProperty = new SimpleStringProperty("");
+        SimpleObjectProperty<LocalDate> dateProperty = new SimpleObjectProperty<>();
 
         SimpleStringProperty subjectProperty = new SimpleStringProperty(subject);
 
@@ -29,25 +82,68 @@ public class ReminderTest {
         Reminder reminder = new Reminder(subject, description, time, date);
         SimpleStringProperty subjectFromPropertyGetter = reminder.getSubjectProperty();
         assertEquals(subjectProperty.get(), subjectFromPropertyGetter.get());
-//        assertTrue(subjectProperty.equals(subjectFromPropertyGetter));
-        //assertTrue(subjectProperty == subjectFromPropertyGetter);
 
     }
 
+
     @Test
     public void getDescriptionProperty() throws Exception {
+        String subject = "titel";
+        String description = "description fo the Reminder";
+        String time = "12:22";
+        LocalDate date = LocalDate.of(2017, 3, 4);
+
+        SimpleStringProperty descriptionProperty = new SimpleStringProperty(description);
+        SimpleStringProperty timeProperty = new SimpleStringProperty(time);
+        SimpleObjectProperty<LocalDate> dateProperty = new SimpleObjectProperty<>(LocalDate.MAX);
+
+        SimpleStringProperty subjectProperty = new SimpleStringProperty(subject);
+
+
+        Reminder reminder = new Reminder(subject, description, time, date);
+        SimpleStringProperty descriptionFromPropertyGetter = reminder.getDescriptionProperty();
+        assertEquals(descriptionProperty.get(), descriptionFromPropertyGetter.get());
     }
 
     @Test
     public void getTimeProperty() throws Exception {
+        String subject = "titel";
+        String description = "description fo the Reminder";
+        String time = "12:22";
+        LocalDate date = LocalDate.of(2017, 3, 4);
+
+        SimpleStringProperty descriptionProperty = new SimpleStringProperty(description);
+        SimpleStringProperty timeProperty = new SimpleStringProperty(time);
+        SimpleObjectProperty<LocalDate> dateProperty = new SimpleObjectProperty<>(LocalDate.MAX);
+
+
+
+        Reminder reminder = new Reminder(subject, description, time, date);
+        SimpleStringProperty timeFromPropertyGetter = reminder.getTimeProperty();
+        assertEquals(timeProperty.get(), timeFromPropertyGetter.get());
     }
 
     @Test
     public void getDateProperty() throws Exception {
+        String subject = "titel";
+        String description = "description fo the Reminder";
+        String time = "12:22";
+        LocalDate date = LocalDate.of(2017, 3, 4);
+
+        SimpleStringProperty descriptionProperty = new SimpleStringProperty(description);
+        SimpleStringProperty timeProperty = new SimpleStringProperty(time);
+        SimpleObjectProperty<LocalDate> dateProperty = new SimpleObjectProperty<>(date);
+
+
+
+        Reminder reminder = new Reminder(subject, description, time, date);
+        SimpleObjectProperty<LocalDate> dateFromPropertyGetter = reminder.getDateProperty();
+        assertTrue(dateProperty.get().isEqual(dateFromPropertyGetter.get()));
     }
 
     @Test
     public void getSerializable() throws Exception {
+
     }
 
     @Test
