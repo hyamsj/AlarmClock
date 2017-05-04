7622497cfe631609489ac935889532aa7ec445f6
pascal <schmp6@bfh.ch>
2017-05-04 15:35:02 +0200

    made normal Reminder serializable

--- src/main/java/alarmClock/model/Reminder.java
+++ src/main/java/alarmClock/model/Reminder.java
@@ -2,8 +2,6 @@ package alarmClock.model;
 
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.ObservableList;
-import jdk.nashorn.internal.objects.annotations.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -14,11 +12,15 @@ import java.time.LocalDateTime;
  */
 public class Reminder implements Serializable{
 
-    private SimpleStringProperty subject = new SimpleStringProperty("");
-    private SimpleStringProperty description = new SimpleStringProperty("");
-    private SimpleObjectProperty <LocalDateTime> time = new SimpleObjectProperty<>();
+    //private SimpleStringProperty subject = new SimpleStringProperty("");
+    private String subject = "";
+    //private SimpleStringProperty description = new SimpleStringProperty("");
+    private String description ="";
+   // private SimpleObjectProperty <LocalDateTime> time = new SimpleObjectProperty<>();
+    private LocalDateTime  time;
     //TODO date is not needed anymore since we have LocalDateTime which has a Date and a Time
-    private SimpleObjectProperty<LocalDate> date = new SimpleObjectProperty<>();
+    //private SimpleObjectProperty<LocalDate> date = new SimpleObjectProperty<>();
+    LocalDate date;
 
     public Reminder(String subject, String description, LocalDateTime time, LocalDate date) {
         setSubject(subject);
@@ -28,36 +30,44 @@ public class Reminder implements Serializable{
     }
 
     public String getSubject() {
-        return subject.get();
+        //return subject.get();
+        return subject;
     }
 
     public SimpleStringProperty getSubjectProperty() {
-        return subject;
+        //return subject;
+        return new SimpleStringProperty(subject);
     }
 
 
     public String getDescription() {
-        return description.get();
+        return description;
+        //return description.get();
     }
 
     public SimpleStringProperty getDescriptionProperty() {
-        return description;
+        return new SimpleStringProperty(description);
+        //return description;
     }
 
     public LocalDateTime getTime() {
-        return time.get();
-    }
-
-    public SimpleObjectProperty<LocalDateTime> getTimeProperty() {
+        //return time.get();
         return time;
     }
 
+    public SimpleObjectProperty<LocalDateTime> getTimeProperty() {
+        //return time;
+        return new SimpleObjectProperty<>(time);
+    }
+
     public LocalDate getDate() {
-        return date.get();
+        //return date.get();
+        return date;
     }
 
     public SimpleObjectProperty<LocalDate> getDateProperty() {
-        return date;
+        //return date;
+        return new SimpleObjectProperty<>(date);
     }
 
     public serializableReminder getSerializable(){
@@ -65,18 +75,22 @@ public class Reminder implements Serializable{
     }
 
     private void setSubject(String subject) {
-        this.subject.set(subject);
+        //this.subject.set(subject);
+        this.subject = subject;
     }
 
     private void setDescription(String description) {
-        this.description.set(description);
+        //this.description.set(description);
+        this.description = description;
     }
     private void setTime(LocalDateTime time) {
-        this.time.set(time);
+        //this.time.set(time);
+        this.time = time;
     }
 
     private void setDate(LocalDate date) {
-        this.date.set(date);
+        //this.date.set(date);
+        this.date = date;
     }
 
 
