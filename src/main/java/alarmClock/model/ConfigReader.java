95f5e74ca05bcd85bece9c394e69e328c045d64c
pascal <schmp6@bfh.ch>
2017-05-23 14:05:33 +0200

    conigFileReader

--- src/main/java/alarmClock/model/ConfigReader.java
+++ src/main/java/alarmClock/model/ConfigReader.java
@@ -6,16 +6,20 @@ import java.util.ArrayList;
 
 /**
  * Created by pascal on 5/17/17.
+ *This is the central place to configure the behavior of the Application.Instead of Hardcoding the boolean values.
+ * It was planed to read a config File and set the boolean values according to the config File.
  *
  */
 public class ConfigReader {
     /**
-     *This is the central place to configure the behavior of the Application.Instead of Hardcoding the boolean values.
-     * It was planed to read a config File and set the boolean values according to the config File.
      * @return the List with the notification types that are used to notify the user
      */
-    boolean enableJavFxNotification= true;
-    boolean enableConsoleNotification= true;
+    private boolean enableJavFxNotification=true;
+    private boolean enableConsoleNotification= true;
+    private boolean enableDarkmode = true;
+    private boolean enablePastReminders = true;
+    private boolean enableImminentReminders = true;
+    private boolean enableRemindersThisMonth = true;
 
     public ArrayList<Notification> getNotificationTypes() {
         ArrayList<Notification> notificationTypes = new ArrayList<>();
@@ -30,13 +34,26 @@ public class ConfigReader {
         } else {
             notificationTypes.remove(new JavaFxNotification());
         }
-
-
         return notificationTypes;
     }
 
     public String getColorScheme() {
-
         return "nightmode";
+    }
+
+    public boolean isEnableDarkmode() {
+        return enableDarkmode;
+    }
+
+    public boolean isEnablePastReminders() {
+        return enablePastReminders;
+    }
+
+    public boolean isEnableImminentReminders() {
+        return enableImminentReminders;
+    }
+
+    public boolean isEnableRemindersThisMonth() {
+        return enableRemindersThisMonth;
     }
 }
